92bce3476d823d3cc611806b6bde0cfee6ae1794
Xing Zhou <xing@xingzhou.me>
2023-04-28 23:21:41 -0400

    feat: render the App using initialState prop

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,21 @@
 import {render, screen} from '@testing-library/react';
-import userEvent from "@testing-library/user-event";
-
 import App from '../src/App';
 
 describe('App', () => {
-    it('renders headline', async () => {
-        const user = userEvent.setup()
-        render(<App/>);
+    describe('renders from initialState', () => {
+        it('count 0', async () => {
+            render(<App initialState={{count: 0}}/>);
+            expect(screen.getByText('count is 0')).toBeInTheDocument()
+        });
 
-        const countButton = screen.getByText('count is 0');
-        expect(countButton).toBeInTheDocument()
+        it('count 1', async () => {
+            render(<App initialState={{count: 1}}/>);
+            expect(screen.getByText('count is 1')).toBeInTheDocument()
+        });
 
-        await user.click(countButton)
-        expect(screen.getByText(/count is 1/)).toBeInTheDocument()
+        it('default value for initial state', async () => {
+            render(<App />);
+            expect(screen.getByText('count is 0')).toBeInTheDocument()
+        });
     });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {PropsWithChildren, useState} from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type AppState = {
+    count: number
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type AppProps = PropsWithChildren<{
+    initialState?: AppState
+}>
+
+function App(props: AppProps) {
+    const initialState = props.initialState ?? {count: 0}
+    const [state] = useState(initialState)
+
+    return (
+        <>
+            <div className="card">
+                <button onClick={() => {/**/
+                }}>
+                    count is {state.count}
+                </button>
+            </div>
+        </>
+    )
 }
 
 export default App
