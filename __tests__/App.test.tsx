d0a65754bdc5b4ce50dc89a70eb1edd1a914b9f4
Xing Zhou <xing@xingzhou.me>
2023-04-29 00:09:26 -0400

    feat: dispatch events in response to clicking the button

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,21 +1,45 @@
 import {render, screen} from '@testing-library/react';
 import App from '../src/App';
+import {vi} from "vitest";
+import userEvent from "@testing-library/user-event";
 
 describe('App', () => {
     describe('renders from initialState', () => {
-        it('count 0', async () => {
+        it('count 0', () => {
             render(<App initialState={{count: 0}}/>);
             expect(screen.getByText('count is 0')).toBeInTheDocument()
         });
 
-        it('count 1', async () => {
+        it('count 1', () => {
             render(<App initialState={{count: 1}}/>);
             expect(screen.getByText('count is 1')).toBeInTheDocument()
         });
 
-        it('default value for initial state', async () => {
-            render(<App />);
+        it('default value for initial state', () => {
+            render(<App/>);
             expect(screen.getByText('count is 0')).toBeInTheDocument()
+        });
+    });
+
+    describe('translates user interactions to events', () => {
+        it('dispatches "increment counter" on click', async () => {
+            const user = userEvent.setup()
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const reducer = vi.fn((state, _) => state);
+            render(<App reducer={reducer}/>);
+
+            await user.click(screen.getByRole('button'))
+
+            expect(reducer).toHaveBeenCalledWith(expect.anything(), {type: 'increment counter'})
+        });
+
+        it('uses default reducer to actually increment the count', async () => {
+            const user = userEvent.setup()
+            render(<App />);
+
+            await user.click(screen.getByRole('button'))
+
+            expect(screen.getByText('count is 1')).toBeInTheDocument()
         });
     });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import {PropsWithChildren, useState} from 'react'
+import {PropsWithChildren, Reducer, useReducer} from 'react'
 import './App.css'
+
+export type Action =
+    | { type: 'increment counter' }
 
 type AppState = {
     count: number
 }
 
+export type AppReducer = Reducer<AppState, Action>
+
 type AppProps = PropsWithChildren<{
-    initialState?: AppState
+    initialState?: AppState,
+    reducer?: AppReducer
 }>
+
+function defaultReducer(state: AppState, action: Action) {
+    switch (action.type) {
+        case "increment counter":
+            return {...state, count: state.count + 1};
+        default:
+            return state
+    }
+}
 
 function App(props: AppProps) {
     const initialState = props.initialState ?? {count: 0}
-    const [state] = useState(initialState)
+    const reducer = props.reducer ?? defaultReducer
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
         <>
             <div className="card">
-                <button onClick={() => {/**/
-                }}>
+                <button onClick={() => dispatch({type: 'increment counter'})}>
                     count is {state.count}
                 </button>
             </div>
